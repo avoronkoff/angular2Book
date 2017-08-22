89ee8f823721d8412ba0e2588ef24b8443224ce3
avoronkoff <voronkoff.aleksei@gmail.com>
2017-08-22 21:39:35 +0300

    fix add task

--- src/app/addtask/app.add-task.component.ts
+++ src/app/addtask/app.add-task.component.ts
@@ -3,6 +3,7 @@ import { Task } from '../shared/interface/task';
 import { TaskService } from '../shared/app.shared';
 import { CanActivateGuard } from '../shared/app.shared';
 import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-task',
@@ -12,12 +13,14 @@ import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
 export class AppAddTaskComponent {
   complexForm: FormGroup;
   task: Task;
+  pattern = /(\d{4})\.(\d{2})\.(\d{2})/;
 
-  constructor(private taskService: TaskService, fb: FormBuilder, private canActivateGuard: CanActivateGuard) {
+  constructor(private taskService: TaskService, fb: FormBuilder, private canActivateGuard: CanActivateGuard,
+              private router: Router) {
     this.defaultTask();
     this.complexForm = fb.group({
       'name' : [null, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(10)])],
-      'deadline' : [null, Validators.required],
+      'deadline' : [null, Validators.pattern(this.pattern)],
       'pomodorosRequired': [null, Validators.compose([Validators.required, Validators.min(0), Validators.max(1000)])],
     });
   }
@@ -31,14 +34,10 @@ export class AppAddTaskComponent {
     };
   }
 
-  canDeactivate() {
-    return window.confirm('change page?');
-  }
-
   addTask(task: any) {
-    task.deadline = new Date(task.deadline.format('YYYY.MM.DD'));
+    task.deadline = new Date(task.deadline.toString());
     this.taskService.addTask(task).subscribe();
     this.canActivateGuard.validateForm(this.complexForm.valid);
-    this.complexForm.reset();
+    this.router.navigate(['/tasks']);
   }
 }
